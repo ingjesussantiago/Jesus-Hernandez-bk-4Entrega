1387b21d88a9a3f42dc1c6f28c2e098ebf434517
Jesus Santiago Hernandez Lopez <ingjesussantiago@gmail.com>
2023-07-26 11:58:43 -0600

    configurando multer

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,34 @@
 import { dirname } from "path"
-import {  fileURLToPath} from "url"
+import { fileURLToPath } from "url"
 
 
-export const __dirname= dirname(fileURLToPath(import.meta.url))
+export const __dirname = dirname(fileURLToPath(import.meta.url))
+
+
+
+
+// Configuracion MULTER
+const storage = multer.diskStorage(
+    {
+
+        destination: function (req, file, cb) {
+            cb(null, `${__dirname}/public/img`)
+        },
+
+
+        filename: function (req, file, cb) {
+
+            cb(null, file.originalname)
+
+        }
+    }
+)
+
+export const uploader = multer({
+    storage,
+
+    onError: function (err, next) {
+        console.log(err);
+        next();
+    }
+});
